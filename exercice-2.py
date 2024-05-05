27933d303207695a528f1f4d8f07ab8142e2748f
Franco <futbolfranconicolas@gmail.com>
2024-05-05 17:40:20 -0300

    Mi primer commit

---
+++ exercice-2.py
@@ -0,0 +1,14 @@
+'''Definir una función max_de_tres(), que tome tres números como argumentos y devuelva el
+mayor de ellos. '''
+
+##def max_de_tres(a, b, c):
+##    maximo = ""
+##    if a > b and a > c:
+##        maximo += str(a)
+##    elif b > a and b > c:
+##        maximo += str(b)
+##    else:
+##        maximo += str(c)
+##    return maximo
+##
+##print(max_de_tres(9, 2, 1))
