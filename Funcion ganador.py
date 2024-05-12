e0c5efed97c946642c1353b7c8e00e7e2d81f713
Franco <futbolfranconicolas@gmail.com>
2024-05-12 19:18:20 -0300

    Mi commit

---
+++ Funcion ganador.py
@@ -0,0 +1,15 @@
+##def ganador(clientes, puntos):
+##    ganador = ""
+##    indice_mayor = ""
+##    punto_mayor = 0
+##    for i in range(len(puntos)):
+##        if puntos[i] > punto_mayor:
+##            punto_mayor = puntos[i]
+##            indice_mayor = i
+##    ganador = clientes[indice_mayor]
+##    return ganador
+##clientes = ["Luis", "Julieta", "Hernan", "Lara"]
+##puntos = [1200, 45, 1240, 50]
+##print(ganador(clientes, puntos))
+
+
